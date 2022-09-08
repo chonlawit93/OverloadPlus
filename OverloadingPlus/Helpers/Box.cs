96e8790fdd2e23ded52f2745ad44a43f14fcd9bd
chonlawitwith-pettersen <s311596@oslomet.no>
2022-09-08 17:34:01 +0200

    Done with this tutorial

---
+++ OverloadingPlus/Helpers/Box.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace OverloadingPlus.Helpers
+{
+    class Box
+    {
+        public double Weight { get; set; }
+        public double Value { get; set; }
+        public double Volume { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is Box box &&
+                   Weight == box.Weight &&
+                   Value == box.Value &&
+                   Volume == box.Volume;
+        }
+
+        public static Box operator +(Box lhs, Box rhs)
+        {
+            return new Box
+            {
+                Value = lhs.Value + rhs.Value,
+                Volume = lhs.Volume + rhs.Volume,
+                Weight = lhs.Weight + rhs.Weight
+            };
+        }
+
+
+
+    }
+}
+
---
+++ OverloadingPlus/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OverloadingPlus.Helpers;
+
+Console.WriteLine("Hello, World!");
+Box box01cust01 = new Box
+{
+    Weight = 100,
+    Volume = 6,
+    Value = 10000
+};
+
+
+Box box02cust02 = new Box
+{
+    Weight = 300,
+    Volume = 15,
+    Value = 4000
+};
+
+
+bool equal = box01cust01.Equals(box02cust02);
+
+
+//Box totalBox = new Box
+//{
+//    Value = box01cust01.Value + box02cust02.Value,
+//    Volume = box01cust01.Volume + box02cust02.Volume,
+//    Weight = box01cust01.Weight + box02cust02.Weight
+//};
+
+// This can be used in calculating total primary attribute in the assignment
+Box totalBox = box01cust01 + box02cust02;
+
+CalculateInsurance(totalBox);
+
+
+
+static void CalculateInsurance(Box total)
+{
+    Console.WriteLine(total.Value * total.Volume / total.Weight);
+}
